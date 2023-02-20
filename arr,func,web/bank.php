cb139e95c2b3a7b344ca0b1da6b542e7a660c22a
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-20 17:59:56 -0300

    adding html in bank.php

--- arr,func,web/bank.php
+++ arr,func,web/bank.php
@@ -22,14 +22,39 @@ $accounts['123.258.265-10'] = [
     'balance' => 1200,
 ];
 
+//retira um item da lista
+unset($accounts['987.654.321-10']);
+
 $accounts['123.456.789-10'] = deposit($accounts['123.456.789-10'], 500);
 $accounts['123.456.789-10'] = withdraw($accounts['123.456.789-10'], 900);
 $accounts['123.456.789-10'] = deposit($accounts['123.456.789-10'], -500);
 $accounts['123.456.789-10'] = withdraw($accounts['123.456.789-10'], -900);
 
 foreach ($accounts as $key => $value) {
-    echo "CPF: {$key}" . PHP_EOL;
-    echo "Nome: {$value['name']} - Salario: {$value['balance']}" .
-        PHP_EOL .
-        PHP_EOL;
+    echo "CPF: {$key}";
+    echo "Nome: {$value['name']} - Salario: {$value['balance']}";
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+
+    <dl>
+        <?php foreach($accounts as $key => $account) { ?>
+        <dt>
+            <h3><?= $account['name']; ?> - <?= $key; ?></h3>
+        </dt>
+        <dd>Saldo: R$<?= $account['balance']; ?></dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
---
+++ arr,func,web/list.php
@@ -0,0 +1,8 @@
+<?php
+
+$ages = [12,13,14];
+list($ViniciusAge, $CamillaAge, $JoaoAge) = $ages;
+
+echo $ViniciusAge . PHP_EOL;
+echo $CamillaAge . PHP_EOL;
+echo $JoaoAge;
